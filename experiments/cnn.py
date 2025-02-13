90d80786130cf4b0683a614245430c3b6c2b159e
dawit-getahun-mengistu <dawitgetahunmengistu@gmail.com>
2025-02-13 20:57:33 +0300

    add expt 6

--- experiments/cnn.py
+++ experiments/cnn.py
@@ -26,3 +26,36 @@ class _2LayerCNN(nn.Module):
         x = nn.functional.relu(x)
         x = self.fc2(x)
         return x
+
+
+class _3LayerCNN(nn.Module):
+
+    def __init__(self, input_channels=3):
+        super(_3LayerCNN, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_channels=input_channels, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(
+            in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(
+            in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
+
+        # self.fc1 = nn.Linear(128 * 3 * 3, 128)
+        self.fc1 = nn.Linear(128 * 4 * 4, 128)
+
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = nn.functional.relu(x)
+        x = nn.functional.max_pool2d(x, 2)
+        x = self.conv2(x)
+        x = nn.functional.relu(x)
+        x = nn.functional.max_pool2d(x, 2)
+        x = self.conv3(x)
+        x = nn.functional.relu(x)
+        x = nn.functional.max_pool2d(x, 2)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = nn.functional.relu(x)
+        x = self.fc2(x)
+        return x
